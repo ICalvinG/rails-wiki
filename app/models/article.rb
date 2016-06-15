8310fce415a6aeadc0461d32bc380568968011f3
notjoeyoo <joseph_yoo@brown.edu>
2016-06-15 16:36:14 -0400

    add snapshot asscociation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ActiveRecord::Base
   has_many :categories, through: :articles_category
+  has_many :snapshots
 
   validates :title, presence: true, length: {maximum: 20}
 end
--- app/models/snapshot.rb
+++ app/models/snapshot.rb
@@ -1,5 +1,7 @@
 class Snapshot < ActiveRecord::Base
 
+  belongs_to :article
+
   validates :body, presence: true
 
 end
