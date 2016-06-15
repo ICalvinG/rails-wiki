a42c998a4fe198b68d92cf8d62ea85ae1becdc48
notjoeyoo <joseph_yoo@brown.edu>
2016-06-15 16:08:13 -0400

    add controllers and some views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,33 @@
 class CategoriesController < ApplicationController
+
   def index
+    @categories = Category.all
   end
 
-  def
+  def new
+    @category = Category.new
+  end
+
+  def create
+    category = Category.create(name: params[:name])
+
+    # ADMIN CAN ONLY CREATE A CATEGORY
+    if category
+      redirect category_url(category)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @category = Category.find(params[:id])
+    @articles = @category.articles
+
+    render 'show'
+  end
+
+  def destroy
+    Category.find(params[:id])
+  end
+
 end
