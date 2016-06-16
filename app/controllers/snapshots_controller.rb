2c3aae1ed8add614e3a70af1c31f65c4673a44b7
ICalvinG <calvinieong1118@gmail.com>
2016-06-16 15:37:52 -0400

    Working on Snapshot show

--- app/controllers/snapshots_controller.rb
+++ app/controllers/snapshots_controller.rb
@@ -1,2 +1,10 @@
 class SnapshotsController < ApplicationController
+
+	def show
+		@article = Article.find(params[:id])
+    	@snapshot = Snapshot.find(@article.snapshots.last.id)
+
+		render 'show'
+	end
+
 end
