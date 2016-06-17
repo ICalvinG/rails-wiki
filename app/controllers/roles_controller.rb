5e4607682926447ddca5f04a0a8fec3c7fd9c336
Ilya Yampolsky <cfnils@gmail.com>
2016-06-17 14:53:40 -0400

    admin can switch user admin roles

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,6 +1,8 @@
 class RolesController < ApplicationController
   before_action :authenticate_user!, exception: [:index, :show, :create]
-
+def index
+	@wiki = Wiki.find_by(params[:wiki])
+end
 
 
 end
